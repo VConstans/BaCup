e1864506e7f6cf9258e985dafac899464d7fef6f
Victor <victor.constans@etu.unistra.fr>
2016-11-27 01:42:40 +0100

    Mise des fonctions des gestion de liste chainée dans un fichier dédié

---
+++ fctListe.c
@@ -0,0 +1,38 @@
+#include "sauve.h"
+
+void addBuff(struct bufferDossier* maillon,struct bufferDossier* buff)
+{
+	struct buffer* tmp=buff;
+	while(tmp->suivant!=NULL)
+	{
+		tmp=tmp->suivant;
+	}
+	tmp->suivant=maillon;
+}
+
+//TODO creermaillon
+
+
+void rmBuff(struct buffer* buff)
+{
+	struct buffDossier* tmp=buff;
+	buff=buff->suivant;
+	free(tmp->chemin);
+	free(tmp);
+}
+
+
+struct bufferDossier* creerMaillon(char* path)
+{
+	struct bufferDossier* maillon;
+	if((maillon=(struct buffferDossier*)malloc(sizeof(struct bufferDossier)))==-1)
+	{
+		perror("Erreur malloc maillon");
+		exit(EXIT_FAILURE);
+	}
+
+	maillon->chemin=(char*)malloc(strlen(path)*sizeof(char));
+	strcpy(maillon->chemin,path);
+
+	return maillon;
+}
